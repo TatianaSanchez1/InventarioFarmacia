a968b36cf3573c69ad8323b646c8803e2ec02c39
Tatiana Sanchez <tatiana.sanchez.sanin@gmail.com>
2022-03-20 13:29:49 -0500

    Login

--- index.php
+++ index.php
@@ -6,10 +6,15 @@
     <title>Sistema de Inventario Farmacéutico</title>
 
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/styleLogin.css">
+
 
 </head>
 
-<body style="background: #262626;">
+<body style="background-image: url(assets/img/bg.jpg); 
+    background-size: cover; 
+    background-position: top; 
+    background-repeat: no-repeat; ">
     <br />
     <br />
 
@@ -17,7 +22,7 @@
 
         <div id="login-form">
 
-            <h3>Iniciar sesión</h3>
+            <h1>Iniciar sesión</h1>
 
             <fieldset>
 
@@ -31,9 +36,18 @@
 
             </fieldset>
 
-        </div> <!-- end login-form -->
+        </div> 
 
     </div>
+
+    <script type="text/javascript">
+        $(".opciones-02").click(function() {
+            $('form').animate({
+                height: "toggle",
+                opacity: "toggle"
+            }, "slow");
+        });
+    </script>
 
     <script type="text/javascript" src="assets/js/jquery-1.12.3.js"></script>
     <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
