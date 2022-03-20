f9986e74151dc6c18acc3c19d77e193eb27c2e66
Tatiana Sanchez <tatiana.sanchez.sanin@gmail.com>
2022-03-20 11:36:48 -0500

    Database connection

---
+++ database/Connection.php
@@ -0,0 +1,31 @@
+<?php 
+class Connection{
+
+	protected $isConn;
+	protected $datab;
+	protected $transaction;
+
+	public function __construct($username="root", $password ="", $host="localhost", $dbname="inventario_farmacia", $options = []){
+		
+		$this->isConn = TRUE;
+		try{
+			$this->datab = new PDO("mysql:host={$host};  dbname={$dbname}; charset=utf8", $username, $password, $options);
+			$this->datab->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$this->transaction = $this->datab;
+			$this->datab->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+			//echo 'Connected Successfully!!!';
+
+		}catch(PDOException $e){
+			throw new Exception($e->getMessage());			
+		}
+
+	}//Fin del Constructor 
+ 
+
+	//Desconectar de la base de datos
+	public function Disconnect(){
+		$this->datab = NULL;//Cerrar conexion en PDO
+		$this->isConn = FALSE;
+	}
+}
+
---
+++ database/Database.php
@@ -0,0 +1,96 @@
+<?php 
+
+include_once('Connection.php');
+
+class Database extends Connection{
+	public function __construct(){
+
+		parent::__construct();
+		if(session_status() == PHP_SESSION_NONE)
+		{
+			session_start();//Iniciar Sesion si no está iniciada
+		}
+	}//Fin del constructos
+
+	public function getRow($query, $params = []){
+		try {
+			$stmt = $this->datab->prepare($query);
+			$stmt->execute($params);
+			return $stmt->fetch();	
+		} catch (PDOException $e) {
+			throw new Exception($e->getMessage());	
+		}
+	}
+
+	public function getRows($query, $params = []){
+		try {
+			$stmt = $this->datab->prepare($query);
+			$stmt->execute($params);
+			return $stmt->fetchAll();	
+		} catch (PDOException $e) {
+			throw new Exception($e->getMessage());	
+		}
+	}
+
+    //Agregar filas
+	public function insertRow($query, $params = []){
+		try {
+			$stmt = $this->datab->prepare($query);
+			$stmt->execute($params);
+			return TRUE;	
+		} catch (PDOException $e) {
+			throw new Exception($e->getMessage());	
+		}
+
+	}
+
+	//Actualizar filas
+	public function updateRow($query, $params = []){
+		$this->insertRow($query, $params);
+		return true;
+	}
+
+	//Eliminar filas
+	public function deleteRow($query, $params = []){
+		$this->insertRow($query, $params);
+		return true;
+	}
+
+	//get ultimo id
+	public function lastID(){
+		$lastID = $this->datab->lastInsertId(); 
+		return $lastID;
+	}
+
+	public function transInsert($query, $params = [], $query2, $params2 = []){
+		try {
+			$this->transaction->beginTransaction();
+				$stmt = $this->datab->prepare($query);
+				$stmt->execute($params);
+
+				$stmt2 = $this->datab->prepare($query2);
+				$stmt2->execute($params2);
+
+			$this->transaction->commit();
+		} catch (PDOException $e) {
+			$this->transaction->rollBack();
+			throw new Exception($e->getMessage());	
+		}
+	}//end transac func
+
+	public function Begin(){
+		$this->transaction->beginTransaction();
+	}
+
+	public function Commit(){
+		$this->transaction->commit();
+	}
+
+	public function test()
+	{
+		echo 'database class test';
+	}
+}
+
+
+ ?>
