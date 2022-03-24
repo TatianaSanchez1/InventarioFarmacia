65bb591d4f6ed4235f22b83c261c0bcba77628f9
Tatiana Sanchez <tatiana.sanchez.sanin@gmail.com>
2022-03-24 00:14:45 -0500

    database connection

--- database/Connection.php
+++ database/Connection.php
@@ -1,31 +1,37 @@
-<?php 
-class Connection{
+<?php
+class Connection
+{
 
 	protected $isConn;
 	protected $datab;
 	protected $transaction;
 
-	public function __construct($username="root", $password ="", $host="localhost", $dbname="inventario_farmacia", $options = []){
-		
+	public function __construct(
+		$username = "root",
+		$password = "",
+		$host = "localhost",
+		$dbname = "salud_noire",
+		$options = []
+	) {
+
 		$this->isConn = TRUE;
-		try{
+		try {
 			$this->datab = new PDO("mysql:host={$host};  dbname={$dbname}; charset=utf8", $username, $password, $options);
 			$this->datab->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			$this->transaction = $this->datab;
 			$this->datab->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 			//echo 'Connected Successfully!!!';
 
-		}catch(PDOException $e){
-			throw new Exception($e->getMessage());			
+		} catch (PDOException $e) {
+			throw new Exception($e->getMessage());
 		}
+	} //Fin del Constructor 
 
-	}//Fin del Constructor 
- 
 
 	//Desconectar de la base de datos
-	public function Disconnect(){
-		$this->datab = NULL;//Cerrar conexion en PDO
+	public function Disconnect()
+	{
+		$this->datab = NULL; //Cerrar conexion en PDO
 		$this->isConn = FALSE;
 	}
 }
-
